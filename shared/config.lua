b98a7a34c02b15049d99438ad62ac03ccc5fb275
AdrianCeku <cekuadrian4@gmail.com>
2024-09-28 05:35:19 +0200

    8x8

--- shared/config.lua
+++ shared/config.lua
@@ -2,7 +2,7 @@ Config = {}
 
 Config.maxWeight = 100000
 
-Config.inventorySizeX = 10
-Config.inventorySizeY = 10
+Config.inventorySizeX = 8
+Config.inventorySizeY = 8
 Config.setAllItemSizesTo1x1 = false -- Effectively disables the tile based inventory aproach
 
